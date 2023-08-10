d2f9dd88d90489315314dbaacbe2ee6a9a4460ba
AdamB25 <barsialtunit@gmail.com>
2023-08-09 21:42:00 -0400

    us 05 query

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -22,3 +22,16 @@ SELECT * from books where name='Book Borrow 2';
 SELECT b.name,b_c.name from books b
                                 inner join book_categories b_c
                                            on b.book_category_id=b_c.id where b.name='Book Borrow 2';
+--US05
+
+-- I have the most popular book's id
+select book_id from book_borrow group by book_id order by count(*) desc limit 1;
+
+-- in order to get the category name I need to find the category id of that book
+
+select book_category_id from books where id=(select book_id from book_borrow group by book_id order by count(*) desc limit 1);
+
+select name from book_categories
+where id=(select book_category_id from books
+          where id=(select book_id from book_borrow group by book_id order by count(*) desc limit 1))
+;
