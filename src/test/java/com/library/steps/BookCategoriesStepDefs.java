307ed4ce0c9c7bb26fb259938d8636d7ad5f9093
AdamB25 <barsialtunit@gmail.com>
2023-08-09 21:57:02 -0400

    us 05 done

--- src/test/java/com/library/steps/BookCategoriesStepDefs.java
+++ src/test/java/com/library/steps/BookCategoriesStepDefs.java
@@ -10,10 +10,17 @@ import org.junit.Assert;
 import java.util.List;
 
 public class BookCategoriesStepDefs {
-    BookPage bookPage=new BookPage();
+    BookPage bookPage;
+  //  BookPage bookPage=new BookPage();
+    /*
+    if we initialize our pages at the class level and run DB we will have one blank browser why ?
+    We are calling constructor of Base age and inside the constructor we are calling Driver.getDriver() method and
+    this method is opening one empty browser for us.
+     */
 
     @When("the user navigates to {string} page")
     public void the_user_navigates_to_page(String moduleName) {
+        bookPage=new BookPage();
         bookPage.navigateModule(moduleName);
         BrowserUtil.waitFor(2);
 
@@ -43,5 +50,21 @@ public class BookCategoriesStepDefs {
 
         Assert.assertEquals(expectedCategories,actualCategories);
 
+    }
+
+    @When("I execute query to find most popular book genre")
+    public void i_execute_query_to_find_most_popular_book_genre() {
+      String queryOfMostPopularCategory="select name from book_categories\n" +
+              "            where id=(select book_category_id from books\n" +
+              "             where id=(select book_id from book_borrow group by book_id order by count(*) desc limit 1))\n" +
+              ";";
+      DB_Util.runQuery(queryOfMostPopularCategory);
+    }
+    @Then("verify {string} is the most popular book genre.")
+    public void verify_is_the_most_popular_book_genre(String expectedFromUser) {
+        String actualFromDB = DB_Util.getFirstRowFirstColumn();
+        System.out.println("actualFromDB = " + actualFromDB);
+        Assert.assertEquals(expectedFromUser,actualFromDB);
+        System.out.println("expectedFromUser = " + expectedFromUser);
     }
 }
--- src/test/resources/features/us05.feature
+++ src/test/resources/features/us05.feature
@@ -1,7 +1,11 @@
 
 Feature: As a data consumer, I want to know genre of books are being borrowed the most
 
+  @db @us05
   Scenario: verify the the common book genre that’s being borrowed
     Given Establish the database connection
     When I execute query to find most popular book genre
     Then verify "Fantasy" is the most popular book genre.
+   # When execute query "select name from book_categories  where id=(select book_category_id from books  where id=
+   # (select book_id from book_borrow group by book_id order by count(*) desc limit 1)) ;"
+  #if you want you can provide your query from feature fie
